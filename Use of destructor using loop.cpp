1b47ac849a07454a7cda2d0b81bfde73117f73a3
Vikas Jayavarapu <vikasjayavarapu12@gmail.com>
2025-10-01 11:42:11 +0530

    Create Use of destructor using loop.cpp

---
+++ Use of destructor using loop.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Date{
+    public:
+    int d = 26;
+    int m = 8;
+    int y = 2025;
+    
+    ~Date(){
+        cout << "Destructor called" << endl;
+    }
+};
+int main(){
+    Date d1,d2,d3,d4;
+    int i;
+    for(i=0;i<4;i++){
+        Date i;
+    }
+    return 0;
+}
+
+
+/*OUTPUT
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+
+  */
